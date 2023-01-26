83b923a46383b94f869736cd5989113fcac695bd
helloalexis <98498554+helloalexis@users.noreply.github.com>
2023-01-26 15:14:07 +0800

    Updated html, css, and js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const section_one_img = document.getElementById("section-one-img");
 const header_link = document.getElementsByClassName("link");
 const flex_container_two = document.getElementsByClassName("flex-container-two");
 const container_three = document.getElementsByClassName("container-three");
+const ham_container = document.getElementsByClassName("ham-container");
+const ham_menu_icon = document.getElementsByClassName("ham-menu-icon");
+
+var menuIsOpen = false;
 
 initial_check();
 
@@ -10,6 +14,7 @@ function initial_check(){
     if (window.innerWidth < 966){
         container_one[0].style.float = "none";
         container_one[0].style.width = "100%"
+        ham_menu_icon[0].style.display = "inline";
 
         for(var i = 0; i < header_link.length; i++){
             header_link[i].style.display = "none";
@@ -23,6 +28,9 @@ function initial_check(){
     }else{
         container_one[0].style.float = "left";
         container_one[0].style.width = "50%"
+        ham_menu_icon[0].style.display = "none";
+        ham_container[0].style.display = "none";
+
         for(var i = 0; i < header_link.length; i++){
             header_link[i].style.display = "inline";
         }
@@ -37,6 +45,7 @@ window.addEventListener("resize", function(){
     if (window.innerWidth < 966){
         container_one[0].style.float = "none";
         container_one[0].style.width = "100%"
+        ham_menu_icon[0].style.display = "inline";
 
         for(var i = 0; i < header_link.length; i++){
             header_link[i].style.display = "none";
@@ -50,6 +59,8 @@ window.addEventListener("resize", function(){
     }else{
         container_one[0].style.float = "left";
         container_one[0].style.width = "50%"
+        ham_menu_icon[0].style.display = "none";
+
         for(var i = 0; i < header_link.length; i++){
             header_link[i].style.display = "inline";
         }
@@ -57,6 +68,18 @@ window.addEventListener("resize", function(){
         flex_container_two[0].style.flexDirection = "row";
         container_three[0].style.flexDirection = "row";
         section_one_img.style.width = "50%";
+        ham_container[0].style.display = "none";
 
     }
 });
+
+function showMenu(){
+    if (!menuIsOpen){
+        ham_container[0].style.display = "flex";
+        menuIsOpen = true;
+    }else{
+        ham_container[0].style.display = "none";
+        menuIsOpen = false;
+    }
+    
+}
